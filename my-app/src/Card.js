407a66ac95ffed12badb9cee6b96ccff428cb34e
Brandon Fiebiger <brandonfiebiger@gmail.com>
2018-07-23 14:59:32 -0600

    Set up toggle hourly extension use placeholder of string of hello temporairily located in card file

--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -1,27 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './Card.css';
 
-const Card = (props) => {
-  const { 
-    day, 
-    hour, 
-    icon, 
-    temp, 
-    high, 
-    low, 
-    condition 
-  } = props;
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showHourly: false
+    }
 
-  return (
-    <article className="Card">
-      {day && <div>{day}</div>}
-      {hour && <div>{hour}</div>}
-      <img alt={condition} src={icon} />
-      {temp && <div>{temp}</div>}
-      {low && <div>{low}{high}</div>}
-    </article>
-  )
+    this.toggleHourly = this.toggleHourly.bind(this);
+  }
+  toggleHourly() {
+    this.setState({
+      showHourly: !this.state.showHourly
+    });
+  }
+
+  render(){ 
+    const { 
+      day, 
+      hour, 
+      icon, 
+      temp, 
+      high, 
+      low, 
+      condition 
+    } = this.props;
+
+    return (
+      <React.Fragment>
+      <article className="Card" onClick={this.toggleHourly}>
+        {day && <div>{day}</div>}
+        {hour && <div>{hour}</div>}
+        <img alt={condition} src={icon} />
+        {temp && <div>{temp}</div>}
+        {low && <div>{low}{high}</div>}
+      </article>
+       {day && this.state.showHourly && <ul>{'Hello'}</ul>}
+      </React.Fragment>
+    )
+  }
 }
 
 export default Card;
